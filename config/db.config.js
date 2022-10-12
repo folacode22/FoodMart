39e753f36ce98548a0b03a934c5dbadaaec1bbbe
Agbabiaka omofolarin <folarinagbabiaka2024@gmail.com>
2022-10-11 22:23:06 -1200

    refactor:database

--- config/db.config.js
+++ config/db.config.js
@@ -2,13 +2,12 @@
 
 
 module.exports = {
-    HOST: "localhost",
-    PORT: 5300,
-    USER:"postgres",
-    PASSWORD: "crunchcode",
-    DB: "foodie",
-    dialect: "postgres",
-
+  HOST: process.env.POSTGRES_HOST,
+  PORT: process.env.POSTGRES_PORT,
+  USER: process.env.POSTGRES_USER,
+  PASSWORD: process.env.POSTGRES_PASSWORD,
+  DB: process.env.POSTGRES_DATABASE,
+  dialect: process.env.POSTGRES_dialect,
 };
 
 
